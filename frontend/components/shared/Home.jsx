5db56bc4c21fbfdfe4b69f13acf68ad6d3e63719
dimitrydesilias <158100419+dimitrydesilias@users.noreply.github.com>
2025-05-19 21:32:19 -0400

    Intégration de Clerk comme système d’authentification pour SysGa admin

---
+++ frontend/components/shared/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="home_inner_title">
+        Une plateforme pour gérer et vérifier vos{" "}
+        <span className="home_inner_title_colored">cartes d'assurance</span> en
+        toute simplicité.
+      </h1>
+      <p className="home_inner_description">
+        SysGa vous permet d'enregistrer et suivre vos cartes d'assurance sur la
+        blockchain pour une expérience transparente et sécurisée.
+      </p>
+    </>
+  );
+}
