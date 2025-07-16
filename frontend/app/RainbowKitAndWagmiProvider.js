11c0b3c50eee8ef27ec149ba27ad70c41e86e757
dimitrydesilias <158100419+dimitrydesilias@users.noreply.github.com>
2025-07-15 20:35:03 -0400

    Enhance RainbowKit and WagmiProvider setup by integrating createConfig for improved network transport configuration and ensuring Sepolia is explicitly defined in the provider.

--- frontend/app/RainbowKitAndWagmiProvider.js
+++ frontend/app/RainbowKitAndWagmiProvider.js
@@ -2,15 +2,15 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
+import { WagmiProvider, createConfig, http } from "wagmi";
 import { sepolia } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const config = getDefaultConfig({
-  appName: "SYSGA Insurance System",
-  projectId: "b7e487bd30ff27535b63f6fbe1b52954",
+const config = createConfig({
   chains: [sepolia],
-  ssr: true,
+  transports: {
+    [sepolia.id]: http(),
+  },
 });
 
 const queryClient = new QueryClient();
@@ -20,6 +20,7 @@ const RainbowKitAndWagmiProvider = ({ children }) => {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider
+          chains={[sepolia]}
           theme={darkTheme({
             accentColor: "#4caf50",
             accentColorForeground: "black",
